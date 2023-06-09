b80ebafefddef64cead4e1f32a805bc4d8e8faa1
Ben Whatling <ben@spring.co.uk>
2023-06-10 00:22:10 +0100

    Move to using repository pattern

--- pkg/handlers/user.go
+++ pkg/handlers/user.go
@@ -1,10 +1,14 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"playground/pkg/models" // replace with your module name
+	"playground/pkg/models"
+	. "playground/pkg/repository"
 )
+
+var userRepo = UserRepositoryImpl{}
 
 // ListUsers PingExample godoc
 // @Summary List users
@@ -16,7 +20,7 @@ import (
 // @Success 200 {object} []models.User
 // @Router /users [get]
 func ListUsers(c *gin.Context) {
-	users := models.GetAllUsers() // Replace with your actual function to retrieve users
+	users := userRepo.GetAllUsers() // Replace with your actual function to retrieve users
 
 	c.JSON(http.StatusOK, gin.H{"data": users})
 }
@@ -37,7 +41,7 @@ func AddUser(c *gin.Context) {
 		return
 	}
 
-	id, err := models.AddUser(user) // Replace with your actual function to add a new user
+	id, err := userRepo.AddUser(user) // Replace with your actual function to add a new user
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not add user"})
 		return
@@ -65,7 +69,7 @@ func UpdateUser(c *gin.Context) {
 		return
 	}
 
-	user, err := models.UpdateUser(id, user) // Replace with your actual function to update a user
+	user, err := userRepo.UpdateUser(id, user) // Replace with your actual function to update a user
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not update user"})
 		return
@@ -85,8 +89,8 @@ func UpdateUser(c *gin.Context) {
 func DeleteUser(c *gin.Context) {
 	id := c.Param("id")
 
-	if err := models.DeleteUser(id); err != nil { // Replace with your actual function to delete a user
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not delete user"})
+	if err := userRepo.DeleteUser(id); err != nil { // Replace with your actual function to delete a user
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Errorf("Could not delete user due to:", err)})
 		return
 	}
 
---
+++ pkg/repository/userRepository.go
@@ -0,0 +1,61 @@
+package repository
+
+import (
+	"fmt"
+	. "playground/pkg/models"
+	"strconv"
+)
+
+type UserRepository interface {
+	GetAllUsers() []User
+	GetUserByID(id string) (User, error)
+	AddUser(user User) (string, error)
+	UpdateUser(id string, newUser User) (User, error)
+	DeleteUser(id string) error
+}
+
+type UserRepositoryImpl struct {
+	Users  []User
+	NextID int
+}
+
+func (u *UserRepositoryImpl) GetAllUsers() []User {
+	return u.Users
+}
+
+func (u *UserRepositoryImpl) GetUserByID(id string) (User, error) {
+	for _, user := range u.Users {
+		if user.ID == id {
+			return user, nil
+		}
+	}
+	return User{}, fmt.Errorf("User not found")
+}
+
+func (u *UserRepositoryImpl) AddUser(user User) (string, error) {
+	user.ID = strconv.Itoa(u.NextID)
+	u.NextID++
+	u.Users = append(u.Users, user)
+	return user.ID, nil
+}
+
+func (u *UserRepositoryImpl) UpdateUser(id string, newUser User) (User, error) {
+	for i, user := range u.Users {
+		if user.ID == id {
+			newUser.ID = id
+			u.Users[i] = newUser
+			return newUser, nil
+		}
+	}
+	return User{}, fmt.Errorf("User not found")
+}
+
+func (u *UserRepositoryImpl) DeleteUser(id string) error {
+	for i, user := range u.Users {
+		if user.ID == id {
+			u.Users = append(u.Users[:i], u.Users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User not found")
+}
